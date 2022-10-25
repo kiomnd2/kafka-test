e5314483a380b0133fcdbe49d25ede5bef5b2678
김형익 <kiomnd2@naver.com>
2022-10-25 23:07:56 +0900

    letch 추가

--- src/main/java/com/example/kafkatest/service/KafkaConsumer.java
+++ src/main/java/com/example/kafkatest/service/KafkaConsumer.java
@@ -7,6 +7,7 @@ import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
+import java.util.concurrent.CountDownLatch;
 
 
 @Slf4j
@@ -16,10 +17,17 @@ public class KafkaConsumer {
     @Value("${kafka.consume.topic}")
     private String topic;
 
+    private CountDownLatch latch = new CountDownLatch(1);
+
+
     @KafkaListener(topics = "${kafka.consume.topic}", groupId = "${kafka.consume.group}",
             containerFactory = "kafkaListenerContainerFactory")
     public void listener(TestModel testModel) {
-        log.debug("test :: {} ", testModel);
+        log.debug("test :: {}, {} ", testModel.getId(), testModel.getName());
+        latch.countDown();
     }
 
+    public CountDownLatch getLatch() {
+        return latch;
+    }
 }
